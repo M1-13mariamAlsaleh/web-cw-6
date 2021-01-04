cf2479b40de4afa5d55c2cdad48c7ee41ac7d441
M1-13mariamAlsaleh <meme.alsaleh1@gmail.com>
2021-01-04 17:09:44 +0300

    سويته مع البونص من امس بس نسيت ادزه :angel:

--- question1/question1.js
+++ question1/question1.js
@@ -1,1 +1,12 @@
 // 👇🏻 قم بحل السؤال الأول هنا
+
+function sayHelloAdvanced(name) {
+    if (name) {
+        console.log(`Hello, ${name}`)
+    }
+    else {
+       console.log("Hello")
+    }
+}
+
+sayHello()
